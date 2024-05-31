68ef4a717f95b33e968532bbbe19cd8796a4b0dc
Jmarc13 <jmarcparagas.doc@gmail.com>
2024-05-31 12:41:03 +0200

    Space between lines

--- hospital/hospital/Application.cs
+++ hospital/hospital/Application.cs
@@ -23,34 +23,48 @@ namespace hospital
                 switch ((eMenuOptions)option)
                 {
                     case eMenuOptions.RegisterDoctor:
+
                         Doctor newDoctor = RequestDataPerson.RequestDataDoctor();
 
                         hospital.RegisterADoctor(newDoctor);
+
                         break;
                     case eMenuOptions.RegisterAPatient:
+
                         Doctor doctorAssigned = RequestDataPerson.RequestDoctorIdentificationAndGet(hospital);
                         Patient newPatient = RequestDataPerson.RequestDataPatient(doctorAssigned);
 
                         hospital.RegisterAPatient(newPatient);
+
                         break;
                     case eMenuOptions.RegisterAAdministrative:
+
                         Administrative newAdministrative = RequestDataPerson.RequestDataAdministrative();
 
                         hospital.RegisterAAdministrative(newAdministrative);
+
                         break;
                     case eMenuOptions.ShowListDoctor:
+
                         Console.WriteLine(hospital.GetListDoctors());
+
                         break;
                     case eMenuOptions.ShowListDoctorPatients:
+
                         Doctor doctor = RequestDataPerson.RequestDoctorIdentificationAndGet(hospital);
-                        Console.WriteLine(doctor.GetListOwnPatients()); 
+                        Console.WriteLine(doctor.GetListOwnPatients());
+
                         break;
                     case eMenuOptions.DeleteAPatient:
+
                         Patient patientToDelete = RequestDataPerson.RequestPatientIdentificationAndGet(hospital);
                         hospital.DeleteAPatientByObject(patientToDelete);
+
                         break;
                     case eMenuOptions.ShowListHospitalPersons:
+
                         Console.WriteLine(hospital.GetListPersons());
+
                         break;
                     default:
                         return;
